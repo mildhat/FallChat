0706e45166e1fad97582651aa4092bd7e9d7d8d6
Yaroslav <yaroslav.aaron@gmail.com>
2017-08-22 12:04:37 +0300

    ccc

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Vault-Tec Chat</title>
+
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+	integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css"
+	integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
+	integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+	<link rel="icon" type="text/css" href="img/favicon.png">
+	
+	<link rel="stylesheet" href="style.css">
+
+	<meta charset="utf-8" name="viewport" content="width=device-width, initial-scale=1">
+
+</head>
+<body>
+    <div class=col-xs-4></div>
+    <div class=col-xs-4><br><br><br><br><br><br>
+        <div class="ss1">
+            <div class=col-xs-6>
+                <form action="sign_in.php" method="post">
+                    <button btn-lg">Sign in</button>
+                </form>
+            </div>
+            <div class=col-xs-6>
+                <form action="sign_up.php" method="post">
+                    <button btn-lg">Sign up</button>
+                </form>
+            </div>
+        </div>
+    </div>
+    <div class=col-xs-4></div>
+</body>
+</html>
