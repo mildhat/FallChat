a7ba0d27b34ee3b3b6902914d35512dac06284e0
Ryuk <tolmachov3@gmail.com>
2017-08-22 23:12:19 +0300

    adapt

--- audio.php
+++ audio.php
@@ -2,20 +2,33 @@
 <html>
 <head>
 	<title>Vault-Tec Chat</title>
+
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+	integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css"
+	integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
+	integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
 	<link rel="icon" type="text/css" href="img/favicon.png">
+
 	<link rel="stylesheet" href="style.css">
-  <link rel="stylesheet" href="styleBS.css">
+
+	<meta charset="utf-8" name="viewport" content="width=device-width, initial-scale=1">
+
 </head>
-<body><br><br><br><br>
-   <div class=col-xs-2></div>
-   <div class=col-xs-2>
-       <ul>
-       	<li><a href="chat.php"><h4>Chat</h4></a></li>
-       	<li><a class="active" href=""><h4>Audio</h4></a></li>
-       	<li><a href="options.php"><h4>Options</h4></a></li>
-       </ul>
-   </div>
-   <div class=col-xs-6>
+
+<body>
+	   <div class=col-xs-2>
+			 <ul>
+				 <li><a href="chat.php">Chat</a></li>
+	       <li><a class="active" href="">Audio</a></li>
+				 <li><a href="options.php">Options</a></li>
+	      </ul>
+	   </div>
+  <div class="col-xs-6">
    <div class="Smenu">
    	   <table>
        <tr>
@@ -62,106 +75,106 @@
       </tr>
       <tr>
         <td>
-          
+
         </td>
       </tr>
       <tr>
       <tr>
           <td>
-            
+
           </td>
       </tr>
       <tr>
         <td>
-          
-        </td>
-      </tr>
-       <tr>
-          <td>
-            
-          </td>
-      </tr>
-      <tr>
-        <td>
-          
-        </td>
-      </tr>
-      <tr>
-      <tr>
-          <td>
-            
-          </td>
-      </tr>
-      <tr>
-        <td>
-          
+
         </td>
       </tr>
        <tr>
           <td>
-            
+
           </td>
       </tr>
       <tr>
         <td>
-          
+
         </td>
       </tr>
       <tr>
       <tr>
           <td>
-            
+
           </td>
       </tr>
       <tr>
         <td>
-          
+
         </td>
       </tr>
        <tr>
           <td>
-            
+
           </td>
       </tr>
       <tr>
         <td>
-          
+
         </td>
       </tr>
       <tr>
       <tr>
           <td>
-            
+
           </td>
       </tr>
       <tr>
         <td>
-          
+
+        </td>
+      </tr>
+       <tr>
+          <td>
+
+          </td>
+      </tr>
+      <tr>
+        <td>
+
+        </td>
+      </tr>
+      <tr>
+      <tr>
+          <td>
+
+          </td>
+      </tr>
+      <tr>
+        <td>
+
         </td>
       </tr>
       <tr>
           <td>
-            
+
           </td>
       </tr>
       <tr>
         <td>
-          
+
         </td>
       </tr>
       <tr>
       <tr>
           <td>
-            
+
           </td>
       </tr>
       <tr>
         <td>
-          
+
         </td>
       </tr>
     </table>
-   </div>
-   <div class=col-xs-2></div>
+    </div>
+  </div>
 </body>
 </html>
