b2664e86641d43b2ed2df15372ac1b50648c80ad
Yaroslav <yaroslav.aaron@gmail.com>
2017-08-28 22:46:42 +0300

    GLOBAL UPDATE

--- chat.php
+++ chat.php
@@ -1,42 +1,30 @@
-<!DOCTYPE html>
-<html>
-<head>
-
-    <title>Vault-Tec Chat</title>
-
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
-    integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css"
-    integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
-
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
-    integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-
-    <meta name="viewport" content="width-device-width, initial-scale-1.0">
-
-    <link rel="icon" type="text/css" href="img/favicon.png">
-
-    <link rel="stylesheet" href="style.css">
-
-    <meta charset="utf-8" name="viewport" content="width=device-width, initial-scale=1">
-
-</head>
-
-<body>
-  <div class=col-xs-2>
-    <ul>
+<?php 
+    session_start();
+    if(!isset($_SESSION["session_username"])) {
+      header("Location: index.php");
+    }
+?>
+<?php include("includes/header.php"); ?>
+    <ul class="menu">
       <li><a class="active" href="">Chat</a></li>
-      <li><a href="audio.php">Audio</a></li>
       <li><a href="options.php">Options</a></li>
+      <li id="hi"><a>Welcome, <span><?php echo $_SESSION['session_username'];?>! </span></a>
+        <ul class="submenu">
+          <li><a href="#">choto</a></li>
+          <li><a href="#">choto</a></li>
+          <li><a href="logout.php">Out</a></li>
+        </ul>
+      </li>
     </ul>
-  </div>
-    <div class="col-xs-6">
+    <div class="col-xs-3"></div>
+      <div class="col-xs-6">
       <div class="inp">
-        <p class="Smenuc"></p>
+        <p class="Smenuc">
+          
+        </p>
         <input id="textM" type="text" size="63" />
         <input id="enterM" name="submitmsg" type="submit" value="Send" />
       </div>
-    </div>
+      </div>
   </body>
 </html>
